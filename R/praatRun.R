a5120f71d3dd02812962b26cd603473c33b2da99
Stefano Coretta <stefanoc.social@me.com>
2017-06-08 18:38:11 +0200

    add stdout capture

--- R/praatRun.R
+++ R/praatRun.R
@@ -2,9 +2,19 @@
 #'
 #' It runs a file containing Praat scripting, with optional arguments.
 #'
-#' @param script caracter vector containing the script name.
-#' @param ... list of arguments of the script to be run.
+#' @param script A character vector containing the script name.
+#' @param ... List of arguments to be passed to the script.
+#' @param capture If set to `TRUE`, the standard output of the script can be
+#' captured into a variable. This is useful if the script uses the "write to Info"
+#' commands.
 #' @export
-praatRun <- function(script, ...) {
-    system2(pkg.env$praat.path, c("--run", c(script, list(...))))
+praatRun <- function(script, ..., capture = FALSE) {
+    if (capture == FALSE) {
+        system2(pkg.env$praat.path, c("--run", c(script, list(...))))
+    } else {
+        output <- system2(pkg.env$praat.path, c("--run", c(script, list(...))),
+                          stdout = capture
+        )
+        return(paste(output, collapse = "\n"))
+    }
 }
