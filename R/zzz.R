6e1602c96e00cf08ccb767ed84762ec3a101a1bc
stefanocoretta <stefano.coretta@gmail.com>
2021-01-20 14:31:30 +0100

    :bug: - set Praat path only if Praat was found

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,10 @@
 .onLoad <- function(libname, pkgname) {
+  praat_path <- speakr:::get_praat_path()
+
+  if (file.exists(praat_path)) {
     set_praat_path(default.path = TRUE)
+    message("Praat found at ", speakr:::get_praat_path())
+  } else {
+   message("Praat not found! Please specify the path to Praat or install it if it's not installed yet.")
+  }
 }
