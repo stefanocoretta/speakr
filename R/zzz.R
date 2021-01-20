4ea6bd3f54261b86f99e1b696f9e8b6286f8fbda
stefanocoretta <stefano.coretta@gmail.com>
2021-01-20 18:45:37 +0100

    :bug: - abide to .onLoad good practices

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,10 @@
-.onLoad <- function(libname, pkgname) {
-  praat_path <- speakr:::get_praat_path()
+.onAttach <- function(libname, pkgname) {
+  praat_path <- get_praat_path()
 
   if (file.exists(praat_path)) {
     set_praat_path(default.path = TRUE)
-    message("Praat found at ", speakr:::get_praat_path())
+    packageStartupMessage("Praat found at ", get_praat_path())
   } else {
-   message("Praat not found! Please specify the path to Praat or install it if it's not installed yet.")
+    packageStartupMessage("Praat not found! Please specify the path to Praat or install it if it's not installed yet.")
   }
 }
