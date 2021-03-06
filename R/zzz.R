e7c3fc80371364e075027c31c3a0592152c7e7db
stefanocoretta <stefano.coretta@gmail.com>
2021-03-06 20:32:47 +0100

    :recycle: - use options to set praat.path (fixes #5)

--- R/zzz.R
+++ R/zzz.R
@@ -1,9 +1,22 @@
+.onLoad <- function(libname, pkgname) {
+  op <- options()
+  op.speakr <- list(
+    speakr.praat.path = praat_path()
+  )
+
+  toset <- !(names(op.speakr) %in% names(op))
+  if (any(toset)) options(op.speakr[toset])
+
+  set_praat_path(getOption("speakr.praat.path"))
+
+  invisible()
+}
+
 .onAttach <- function(libname, pkgname) {
   praat_path <- get_praat_path()
 
   if (file.exists(praat_path)) {
-    set_praat_path(default.path = TRUE)
-    packageStartupMessage("Praat found at ", get_praat_path())
+    packageStartupMessage("Praat found at ", praat_path)
   } else {
     packageStartupMessage("Praat not found! Please specify the path to Praat or install it if it's not installed yet.")
   }
