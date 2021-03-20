5e5e1d1283cc4d10c68ccee7b219e3b88defcf8c
stefanocoretta <stefano.coretta@gmail.com>
2021-03-20 17:43:38 +0100

    :memo: - document work in progress

--- R/plot.R
+++ R/plot.R
@@ -1,17 +1,20 @@
-#' Title
+#' Plot waveform and spectrogram
 #'
-#' @param file
-#' @param wav
-#' @param tg
-#' @param width
-#' @param height
-#' @param pitch
+#' @param file Output file path as a string.
+#' @param wav Wav file path as a string.
+#' @param tg TextGrid file path as a string. If `NULL` (the default), a TextGrid
+#'   is plotted if a TextGrid file with the same name as the wav file is found.
+#'   No TextGrid is plotted otherwise.
+#' @param start Start time of the plotting window in seconds.
+#' @param end End time of the plotting window in seconds. If `NULL` (the
+#'   default), plot the entire duration.
+#' @param width Width of the plot in inches.
+#' @param f0 Whether to plot f0 (`FALSE` by default).
 #'
-#' @return
+#' @return Nothing. It is used for its side effects.
 #' @export
 #'
-#' @examples
-praat_plot <- function(file, wav, tg = NULL, start = 0, end = NULL, width = 5, pitch = NULL) {
+praat_plot <- function(file, wav, tg = NULL, start = 0, end = NULL, width = 5, f0 = FALSE) {
   wd <- getwd()
   file <- file.path(wd, file)
   wav <- normalizePath(wav)
@@ -28,10 +31,10 @@ praat_plot <- function(file, wav, tg = NULL, start = 0, end = NULL, width = 5, p
     }
   }
 
-  if (is.null(pitch)) {
-    pitch <- "FALSE"
-  } else {
+  if (f0) {
     pitch <- "TRUE"
+  } else {
+    pitch <- "FALSE"
   }
 
   if (is.null(end)) {
